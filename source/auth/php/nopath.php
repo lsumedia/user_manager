ac8f9ea6e07f27212d09b2a19842deb56520b895
camerontsmall <camerontsmall@googlemail.com>
2016-07-02 00:04:17 +0100

    added some bs fake key gen for testing

--- source/auth/php/nopath.php
+++ source/auth/php/nopath.php
@@ -1,4 +1,5 @@
 <div>
     <h4>No redirect path provided!</h4>
-    
+    <p>Login succeeded but the server was not provided with a redirect destination.</p>
+    <h5>Your access key is <strong><?= $_GET['key'] ?></strong></h5>
 </div>
--- source/request_login.php
+++ source/request_login.php
@@ -22,8 +22,14 @@ switch($action){
             header('location:./auth/?p=login&error');
         }
         
+        $key = sha1(time());
+        
         if(strlen($redirect) < 1){
-            header('location:' . $config['dashboard_address'] . '?key=5');
+            if(strlen($config['dashboard_address']) > 1){
+                header('location:' . $config['dashboard_address'] . '?key=' . $key);
+            }else{
+                header('location:./auth/?p=nopath&key=' . $key);
+            }
         }
         echo $redirect;
         break;
